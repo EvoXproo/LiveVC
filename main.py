ebd1c95102b5835a90772ec1d5ac8fa648f46e30
initAlishan <initalishan@gmail.com>
2025-08-18 00:43:07 +0530

    1

--- main.py
+++ main.py
@@ -123,11 +123,12 @@ async def show(event):
     
 async def get_chat_id():
     async for msg in client.iter_messages("me", limit=1):
-        try:
-            chat_id = int(msg)
-            return chat_id
-        except Exception:
-            chat_id = None
-            return chat_id
+        if msg and msg.text:
+            try:
+                chat_id = int(msg.text.strip())
+                return chat_id
+            except ValueError:
+                return None
+    return None
     
 client.run_until_disconnected()
